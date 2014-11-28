61aee641f6c251caa22608883bbaddc9b885ec12
SitnicVictor <SitnicVictor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 23:47:31 +0000

    First Commit
    git-svn-id: https://plugins.svn.wordpress.org/clone-maker/trunk@1034801 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,153 @@
+<?php
+load_plugin_textdomain('clonemaker', false, basename( dirname( __FILE__ ) ) . '/languages' );
+function AddBox()  {  
+	add_meta_box( 
+       'CloneBox', // this is HTML id
+       'Clone from Page', 
+       'showCanClonePage', // the callback function
+       'page', // register on post type = page
+       'side', // 
+       'core'
+    );
+}
+add_action ( 'add_meta_boxes_page', 'AddBox');
+
+
+function showCanClonePage( )
+{
+ $args = array(
+	'sort_order' => 'ASC',
+	'sort_column' => 'post_title',
+	'hierarchical' => 1,
+	'exclude' => '',
+	'include' => '',
+	'meta_key' => '',
+	'meta_value' => '',
+	'authors' => '',
+	'child_of' => 0,
+	'parent' => -1,
+	'exclude_tree' => '',
+	'number' => '',
+	'offset' => 0,
+	'post_type' => 'page',
+	'post_status' => 'publish'
+); 
+$pages = get_pages($args); 
+
+echo "<ul>";
+foreach ($pages as $key => $value) {
+		
+	echo "<li>";
+		echo $value->post_title;		
+	echo "<a class='clonepage' style='float:right;text-decoration:none' href='".$value->ID."'>+++</a><hr style='clear:both'/></li>";
+}
+echo "</ul>";
+}
+
+function getMyData(){
+	
+	
+	 $args = array(
+	'sort_order' => 'ASC',
+	'sort_column' => 'post_title',
+	'hierarchical' => 1,
+	'exclude' => '',
+	'include' => '',
+	'meta_key' => '',
+	'meta_value' => '',
+	'authors' => '',
+	'child_of' => 0,
+	'parent' => -1,
+	'exclude_tree' => '',
+	'number' => '',
+	'offset' => 0,
+	'post_type' => 'page',
+	'post_status' => 'publish'
+); 
+$pages = get_pages($args); 
+$myid = $_POST['data'];
+
+$myarray = array('title'=>'','content'=>'');
+foreach ($pages as $key => $value) {
+	
+	if($value->ID == $myid){
+	$myarray['title']=$value->post_title;
+	$myarray['content']=$value->post_content;
+	
+	
+}	
+	
+	
+}
+
+$valj = json_encode($myarray);
+ print_r($valj);
+  die();
+   }
+
+add_action('wp_ajax_getMyData', 'getMyData');
+add_action('wp_ajax_nopriv_getMyData', 'getMyData');
+
+
+add_action( 'admin_footer', 'my_action_javascript' ); // Write our JS below here
+
+function my_action_javascript() { ?>
+	<script type="text/javascript" >
+	
+	function getContent(event){
+		event.preventDefault();
+			myid = jQuery(this).attr('href');
+		
+		    jQuery.ajax({
+            method:'POST',
+            url:"<?=admin_url( 'admin-ajax.php' )?>",
+            data:{
+                 action:'getMyData',
+                 data: myid
+            },
+            success: function(data){
+                  fillFields(data);
+            },
+            error: function(data){return false;}
+        });
+	}
+	
+	function fillFields(mydata){
+		
+		jso = JSON.parse(mydata);
+
+		mycont = jso ['content'];
+		
+		
+		
+		
+		if(jQuery("#wp-content-wrap").hasClass('tmce-active')){
+			z = true;
+	   } else {
+	   	z=false;
+	   }
+	    
+	    
+	   	if(z){jQuery('a#content-html').click()};
+	    
+	    
+		jQuery('textarea[name="content"]').attr('value');
+		jQuery('textarea[name="content"]').attr('value',mycont);
+		
+		if(z){jQuery('a#content-tmce').click()};
+		
+		
+	
+	}
+	
+
+	jQuery('.clonepage').bind("click",getContent);
+	
+	
+	</script> <?php
+}
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,41 @@
+Clonemaker
+==========
+
+"Clonemaker" is an wordpress plugin. Main target of this plugin is to insert in new page content from another page, simulate a copy - paste action.
+
+How to install
+---------------
+
+###First Easy Way
+
+Download `clonemaker.zip` on your PC and remember location where archive a saved.
+
+After that go to your Wordpress admin page, from menu chose `plugins>add new`
+
+Press on `Upload Plugin` button on top near **Add Plugins** heading
+
+Now `Chose File` and show path to downloaded archieve after that press `Install Now` button
+
+After successful upload  you can activate the plugin
+
+
+###Second  Way
+
+Unzip downloaded `clonemaker.zip` 
+
+And copy unzipped files in `wp-content>plugins` folder of  your wordpress folder
+
+After that login to your Wordpress admin page go to `plugins` in menu, and find plugin with "Clone Maker" title and press `Activate`
+
+
+How to use
+---------------
+First of all you must activate it if is not active. Go to your Wordpress admin page , chose `plugins` form menu, in list find plugin with "Clone Maker" title and press `Activate`
+
+Create some Wordpress page in `pages` section if you don't have yet else hit `Add New` button on `pages` section
+now in right part you see the plugin with title `Clone from Page` where is displayed list of available page to get content from it.
+
+![alt text](helpimage/1a.png "Title")
+
+
+Press on `+++` link to get the content from selected page, editor is fit with loaded content from selected page.
