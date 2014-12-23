d3f9f1bcc70f98036a9712f9ac060cb5d7ba4b2d
SitnicVictor <SitnicVictor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 12:22:07 +0000

    Adding clone functionality to post page
    git-svn-id: https://plugins.svn.wordpress.org/clone-maker/trunk@1052526 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- clonemaker.php
+++ clonemaker.php
@@ -1,28 +1,28 @@
-<?php
-defined('ABSPATH') or die("No script kiddies please!");
-/**
- * Plugin Name: Clone Maker
- * Plugin URI:https://github.com/nosstradamus/Clonemaker
- * Description: "Clonemaker" is an wordpress plugin. Main target of this plugin is to insert in new page content from another page, simulate a copy - paste action.
- * Version: 1.0
- * Author:Sitnic Victor
- * Author URI: https://github.com/nosstradamus
- * Text Domain: #
- * Domain Path: #
- * Network: #
- * License: GPL2
- *Copyright 2014  SITNIC VICTOR  (email : sitnic.victor@gmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-*/
- include "functions.php";
+<?php
+defined('ABSPATH') or die("No script kiddies please!");
+/**
+ * Plugin Name: Clone Maker
+ * Plugin URI:https://github.com/nosstradamus/Clonemaker
+ * Description: "Clonemaker" is an wordpress plugin. Main target of this plugin is to insert in new page content from another page, simulate a copy - paste action.
+ * Version: 1.0
+ * Author:Sitnic Victor
+ * Author URI: https://github.com/nosstradamus
+ * Text Domain: #
+ * Domain Path: #
+ * Network: #
+ * License: GPL2
+ *Copyright 2014  SITNIC VICTOR  (email : sitnic.victor@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+*/
+ include "functions.php";
--- functions.php
+++ functions.php
@@ -1,21 +1,37 @@
+
 <?php
+ function CM_connect_style() {
+       /* Register our stylesheet. */
+       wp_register_style( 'myPluginStylesheet', plugins_url('clonemaker.css', __FILE__) );
+       wp_enqueue_style( 'myPluginStylesheet' );
+   }
+add_action( 'admin_init', 'CM_connect_style' );
 load_plugin_textdomain('clonemaker', false, basename( dirname( __FILE__ ) ) . '/languages' );
-function AddBox()  {  
+
+function CM_AddBox()  {  
+
+        $cm_postTypes = array("post","page");
+        
+        foreach($cm_postTypes as $value){
 	add_meta_box( 
-       'CloneBox', // this is HTML id
-       'Clone from Page', 
-       'showCanClonePage', // the callback function
-       'page', // register on post type = page
+       'CM-CloneBox'.ucfirst($value), // this is HTML id
+       'Clone from '.ucfirst($value), 
+       'CM_callList', // the callback function
+       $value, // register on post type = page
        'side', // 
        'core'
+
     );
+        }
+
+
 }
-add_action ( 'add_meta_boxes_page', 'AddBox');
+
+add_action ( 'add_meta_boxes', 'CM_AddBox');
 
 
-function showCanClonePage( )
-{
- $args = array(
+function CM_args($type){
+    $args = array(
 	'sort_order' => 'ASC',
 	'sort_column' => 'post_title',
 	'hierarchical' => 1,
@@ -29,80 +45,82 @@ function showCanClonePage( )
 	'exclude_tree' => '',
 	'number' => '',
 	'offset' => 0,
-	'post_type' => 'page',
+	'post_type' => $type,
 	'post_status' => 'publish'
-); 
-$pages = get_pages($args); 
 
-echo "<ul>";
-foreach ($pages as $key => $value) {
-		
-	echo "<li>";
+); 
+}
+
+
+function CM_showList($val){
+    echo "<ul>";
+        foreach ($val as $key => $value) {
+            
+            
+            
+	echo "<li class='CM-".$value->post_type."'>";
 		echo $value->post_title;		
 	echo "<a class='clonepage' style='float:right;text-decoration:none' href='".$value->ID."'>+++</a><hr style='clear:both'/></li>";
 }
 echo "</ul>";
 }
 
-function getMyData(){
-	
-	
-	 $args = array(
-	'sort_order' => 'ASC',
-	'sort_column' => 'post_title',
-	'hierarchical' => 1,
-	'exclude' => '',
-	'include' => '',
-	'meta_key' => '',
-	'meta_value' => '',
-	'authors' => '',
-	'child_of' => 0,
-	'parent' => -1,
-	'exclude_tree' => '',
-	'number' => '',
-	'offset' => 0,
-	'post_type' => 'page',
-	'post_status' => 'publish'
-); 
-$pages = get_pages($args); 
+function CM_callList( )
+{
+$cm_posts = array_merge(get_pages(CM_args('page')),get_posts(CM_args('post')));
+CM_showList($cm_posts);
+}
+
+
+
+function CM_getMyData(){
+$cm_posts = array_merge(get_pages(CM_args('page')),get_posts(CM_args('post')));
 $myid = $_POST['data'];
 
+
+
 $myarray = array('title'=>'','content'=>'');
-foreach ($pages as $key => $value) {
+
+foreach ($cm_posts as $key => $value) {
 	
 	if($value->ID == $myid){
 	$myarray['title']=$value->post_title;
 	$myarray['content']=$value->post_content;
 	
-	
 }	
-	
+
 	
 }
+
+
 
 $valj = json_encode($myarray);
  print_r($valj);
   die();
+
    }
 
-add_action('wp_ajax_getMyData', 'getMyData');
-add_action('wp_ajax_nopriv_getMyData', 'getMyData');
 
 
-add_action( 'admin_footer', 'my_action_javascript' ); // Write our JS below here
+add_action('wp_ajax_CM_getMyData', 'CM_getMyData');
+add_action('wp_ajax_nopriv_CM_getMyData', 'CM_getMyData');
 
-function my_action_javascript() { ?>
+
+add_action( 'admin_footer', 'CM_script' ); // Write our JS below here
+
+
+
+function CM_script() { ?>
+
 	<script type="text/javascript" >
-	
 	function getContent(event){
 		event.preventDefault();
-			myid = jQuery(this).attr('href');
-		
+			myid = jQuery(this).attr('href');	
 		    jQuery.ajax({
             method:'POST',
             url:"<?=admin_url( 'admin-ajax.php' )?>",
             data:{
-                 action:'getMyData',
+                 action:'CM_getMyData',
                  data: myid
             },
             success: function(data){
@@ -111,41 +129,39 @@ function my_action_javascript() { ?>
             error: function(data){return false;}
         });
 	}
-	
-	function fillFields(mydata){
-		
-		jso = JSON.parse(mydata);
 
+
+
+	function fillFields(mydata){
+		jso = JSON.parse(mydata);
 		mycont = jso ['content'];
-		
-		
-		
-		
 		if(jQuery("#wp-content-wrap").hasClass('tmce-active')){
+
 			z = true;
+
 	   } else {
+
 	   	z=false;
+
 	   }
-	    
-	    
+
 	   	if(z){jQuery('a#content-html').click()};
-	    
-	    
 		jQuery('textarea[name="content"]').attr('value');
 		jQuery('textarea[name="content"]').attr('value',mycont);
-		
 		if(z){jQuery('a#content-tmce').click()};
-		
-		
-	
+
 	}
-	
 
 	jQuery('.clonepage').bind("click",getContent);
-	
-	
 	</script> <?php
+
 }
+
+
+
+
+
+
 
 
 
